753ad26ed601e4168e05e97979e1b52c719cacb3
trakbit <harsh.vardhan.611@gmail.com>
2021-01-17 23:11:48 +0530

    Update settings.py

--- fullstackbackend/settings.py
+++ fullstackbackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')(=7tnns03=p*x!h7q*3c9o!jf%b$^4qx9%_@*oy4%mus_0&2y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['fullstackbot.com']
+ALLOWED_HOSTS = ['fullstackbot-backend.onrender.com']
 CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
