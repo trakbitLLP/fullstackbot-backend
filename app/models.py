02c7c6d9ddddd70e6ed2d3af5e5570769b1d06cc
trakbit <harsh.vardhan.611@gmail.com>
2021-01-18 10:12:27 +0530

    indentation and package update

--- app/models.py
+++ app/models.py
@@ -1,17 +1,17 @@
-
 from django.contrib.postgres.fields import ArrayField
 from datetime import datetime
 from django.db import models
 
+
 class Job(models.Model):
-  company_image = models.TextField()
-  company_name = models.CharField(max_length=400)
-  job_title = models.CharField(max_length=400)
-  job_location = models.CharField(max_length=400)
-  job_link = models.CharField(max_length=400)
-  job_post = models.CharField(
+    company_image = models.TextField()
+    company_name = models.CharField(max_length=400)
+    job_title = models.CharField(max_length=400)
+    job_location = models.CharField(max_length=400)
+    job_link = models.CharField(max_length=400)
+    job_post = models.CharField(
         max_length=400,
         default=str(datetime.today().strftime('%Y-%m-%d')))
-  tags = ArrayField(
+    tags = ArrayField(
             models.CharField(max_length=400, blank=True),
             size=8)
