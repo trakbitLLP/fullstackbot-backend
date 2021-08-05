a6e5c3ab9015ba1bca6daba40cd2f1e8c58be21c
trakbit <harsh.vardhan.611@gmail.com>
2021-08-05 17:16:45 +0530

    Update scraper.py

--- app/scraper.py
+++ app/scraper.py
@@ -4,132 +4,74 @@ from bs4 import BeautifulSoup
 from django.db import connection
 
 
-def scrap_stackoverflow_pg_1():
-    Job.objects.all().filter(automated=True).delete()
-    Tag.objects.all().delete()
-    connection.close()
-    URL = 'https://stackoverflow.com/jobs?dr=FullStackDeveloper'
-    page = requests.get(URL)
+def scrap_stackoverflow_pg():
+    for pg in range(1, 3):
+        Job.objects.all().filter(automated=True).delete()
+        Tag.objects.all().delete()
+        connection.close()
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-    jobs_list = soup.find(class_='listResults')
+        URL = "https://stackoverflow.com/jobs?dr=FullStackDeveloper?pg={}".format(pg)
+        page = requests.get(URL)
 
-    jobs_items = jobs_list.find_all(class_='grid--cell fl1')     
-    jobs_image_items = jobs_list.find_all('img', class_='w48 h48 bar-sm')
+        soup = BeautifulSoup(page.content, 'html.parser')
+        jobs_list = soup.find(class_='listResults')
 
-    company_image = None
-    company_name = None
-    job_title = None
-    job_location = None
-    job_link = None
+        jobs_items = jobs_list.find_all(class_='fl1')   
+        jobs_image_items = jobs_list.find_all('img', class_='s-avatar--image')
 
-    company_images = []
+        company_image = None
+        company_name = None
+        job_title = None
+        job_location = None
+        job_link = None
 
-    for job_image_element in jobs_image_items:
-        company_image = job_image_element["src"]
-        company_images.append(company_image)
+        company_images = []
 
-    for i, job_element in enumerate(jobs_items):
-        if i == len(company_images):
-            break
-        else:
-            tags = []
-            job_title = job_element.find('a', class_='s-link stretched-link').text
-            job_title_anchor = (job_element.find('a', class_='s-link stretched-link'))
-            job_link = 'https://stackoverflow.com'+job_title_anchor['href']
+        for job_image_element in jobs_image_items:
+            company_image = job_image_element["src"]
+            company_images.append(company_image)
 
-            job_page = requests.get(job_link)
-            job_soup = BeautifulSoup(job_page.content, 'html.parser')
-            job_content = str(job_soup.find(class_='mb32 fs-body2 fc-medium pr48'))
+        for i, job_element in enumerate(jobs_items):
+            if i == len(company_images):
+                break
+            else:
+                tags = []
+                job_title = job_element.find('a', class_='s-link stretched-link').text
+                job_title_anchor = (job_element.find('a', class_='s-link stretched-link'))
+                job_link = 'https://stackoverflow.com'+job_title_anchor['href']
 
-            company_and_location = job_element.find('h3', class_='fc-black-700 fs-body1 mb4').text
-            company_name = (company_and_location.split('•')[0]).strip()
-            job_location = (company_and_location.split('•')[1]).strip()
+                job_page = requests.get(job_link)
+                job_soup = BeautifulSoup(job_page.content, 'html.parser')
+                job_content = str(job_soup.find(class_='mb32 fs-body2 fc-medium'))
 
-            tags_element = job_element.find_all('a', class_='post-tag no-tag-menu')
-            for tag in tags_element:
-                try:
-                    tag_obj = Tag.objects.get(tag=tag.text)
-                    tag_obj.count = tag_obj.count + 1
-                    tag_obj.save()
-                except Tag.DoesNotExist:
-                    Tag(tag=tag.text).save()
-                tags.append(tag.text)
-            connection.close()
+                company_and_location = job_element.find('h3', class_='fc-black-700 fs-body1 mb4').text
+                company_name = (company_and_location.split('•')[0]).strip()
+                print(company_name)
+                job_location = (company_and_location.split('•')[1]).strip()
 
-            Job(
-                company_image=company_images[i],
-                company_name=company_name,
-                job_content=job_content,
-                job_title=job_title,
-                job_location=job_location,
-                job_link=job_link,
-                tags=tags,
-            ).save()
+                tags_element = job_element.find_all('a', class_='s-tag')
+                for tag in tags_element:
+                    try:
+                        tag_obj = Tag.objects.get(tag=tag.text)
+                        tag_obj.count = tag_obj.count + 1
+                        tag_obj.save()
+                    except Tag.DoesNotExist:
+                        Tag(tag=tag.text).save()
+                    tags.append(tag.text)
+                connection.close()
 
-
-def scrap_stackoverflow_pg_2():
-    URL = 'https://stackoverflow.com/jobs?dr=FullStackDeveloper?pg=2'
-    page = requests.get(URL)
-
-    soup = BeautifulSoup(page.content, 'html.parser')
-    jobs_list = soup.find(class_='listResults')
-
-    jobs_items = jobs_list.find_all(class_='grid--cell fl1')
-    jobs_image_items = jobs_list.find_all('img', class_='w48 h48 bar-sm')
-
-    company_image = None
-    company_name = None
-    job_title = None
-    job_location = None
-    job_link = None
-
-    company_images = []
-
-    for job_image_element in jobs_image_items:
-        company_image = job_image_element["src"]
-        company_images.append(company_image)
-
-    for j, job_element in enumerate(jobs_items):
-        if j == len(company_images):
-            break
-        else:
-            tags = []
-            job_title = job_element.find('a', class_='s-link stretched-link').text
-            job_title_anchor = (job_element.find('a', class_='s-link stretched-link'))
-            job_link = 'https://stackoverflow.com'+job_title_anchor['href']
-
-            job_page = requests.get(job_link)
-            job_soup = BeautifulSoup(job_page.content, 'html.parser')
-            job_content = str(job_soup.find(class_='mb32 fs-body2 fc-medium pr48'))
-
-            company_and_location = job_element.find('h3', class_='fc-black-700 fs-body1 mb4').text
-            company_name = (company_and_location.split('•')[0]).strip()
-            job_location = (company_and_location.split('•')[1]).strip()
-
-            tags_element = job_element.find_all('a', class_='post-tag no-tag-menu')
-
-            for tag in tags_element:
-                try:
-                    tag_obj = Tag.objects.get(tag=tag.text)
-                    tag_obj.count = tag_obj.count + 1
-                    tag_obj.save()
-                except Tag.DoesNotExist:
-                    Tag(tag=tag.text).save()
-                tags.append(tag.text)
-            connection.close()
-
-            Job(
-                company_image=company_images[j],
-                company_name=company_name,
-                job_content=job_content,
-                job_title=job_title,
-                job_location=job_location,
-                job_link=job_link,
-                tags=tags,
-            ).save()
+                Job(
+                    company_image=company_images[i],
+                    company_name=company_name,
+                    job_content=job_content,
+                    job_title=job_title,
+                    job_location=job_location,
+                    job_link=job_link,
+                    tags=tags,
+                ).save()
 
 
 def scraper():
-    scrap_stackoverflow_pg_1()
-    scrap_stackoverflow_pg_2()
+    scrap_stackoverflow_pg()
+
+# scraper()
